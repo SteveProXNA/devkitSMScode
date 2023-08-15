8ed61ec74ebf8441831fe0b3c9f72cc0f5053c9a
SteveProXNA <steven_boland@hotmail.com>
2023-08-15 15:25:58 +0100

    Upload all files

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # devkitSMS code
-Sega Master System code samples using devkitSMS from StevePro Studios [blog](http://steveproxna.blogspot.com)
+Code samples from [Blogger](https://steveproxna.blogspot.com) articles tagged as [devkitSMS](https://steveproxna.blogspot.com/search/label/devkitSMS).
+
+### devkitSMS Programming Setup
+Fri, 15th September, 2017
+<br />
+https://steveproxna.blogspot.com/2017/09/devkitsms-programming-setup.html
+
+### devkitSMS Programming Setup II
+Sat, 4th June, 2022
+<br />
+https://steveproxna.blogspot.com/2022/06/devkitsms-programming-setup-ii.html
+
+### devkitSMS Programming Sample
+Wed, 15th November, 2017
+<br />
+https://steveproxna.blogspot.com/2017/11/devkitsms-programming-sample.html
+
+### devkitSMS Programming Sample II
+Sat, 17th March, 2018
+<br />
+https://steveproxna.blogspot.com/2018/03/devkitsms-programming-sample-ii.html
+
+### devkitSMS Programming Sample III
+Sun, 28th April, 2019
+<br />
+https://steveproxna.blogspot.com/2019/04/devkitsms-programming-sample-iii.html
+
+### devkitSMS Programming Sample IV
+Sun, 31st May, 2020
+<br />
+https://steveproxna.blogspot.com/2020/05/devkitsms-programming-sample-iv.html
+
+### devkitSMS Programming Sample V
+Mon, 4th July, 2022
+<br />
+https://steveproxna.blogspot.com/2022/07/devkitsms-programming-sample-v.html
--- Utilities/ShellScriptCopy/build.bat
+++ Utilities/ShellScriptCopy/build.bat
@@ -6,18 +6,50 @@ set _time=%time: =0%
 set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
 set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 
-
 :: Compile
+cd banks
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c --no-std-crt0 -mz80 --Werror --opt-code-size --constseg BANK7 fixedbank.c
+cd ..
+
 cd devkit
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
+::::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
+::::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
 cd ..
 
 cd engine
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 enum_manager.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 function_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 global_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 input_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 level_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 screen_manager.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 scroll_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 sprite_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 storage_manager.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 tile_manager.c
 cd ..
 
-sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
+cd object
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 level_object.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 scroll_object.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 storage_object.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-size --peep-file ..\peep-rules.txt --std-c99 tile_object.c
+cd ..
+
+cd screen
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 func_screen.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 none_screen.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 splash_screen.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 test_screen.c
+::"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 title_screen.c
+cd ..
+
+
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
 
 :: Time build -END-
@@ -31,19 +63,51 @@ echo.
 
 
 :: Link
-sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ../crt0/crt0_sms.rel main.rel ^
+-Wl-b_BANK2=0x8000  -Wl-b_BANK3=0x8000 ^
+-Wl-b_BANK4=0x8000  -Wl-b_BANK5=0x8000 ^
+-Wl-b_BANK6=0x8000  -Wl-b_BANK7=0x8000 ^
 ../lib/SMSlib.lib ^
+../lib/PSGlib.rel ^
+banks/bank2.rel  banks/bank3.rel ^
+banks/bank4.rel  banks/bank5.rel ^
+banks/bank6.rel  banks/fixedbank.rel ^
 devkit/_sms_manager.rel ^
+devkit/_snd_manager.rel ^
 engine/asm_manager.rel ^
+engine/audio_manager.rel ^
 engine/content_manager.rel ^
-content/gfx.rel
-
+engine/enum_manager.rel engine/font_manager.rel ^
+engine/function_manager.rel engine/global_manager.rel ^
+engine/input_manager.rel ^
+engine/level_manager.rel ^
+engine/screen_manager.rel ^
+engine/scroll_manager.rel ^
+engine/sprite_manager.rel ^
+engine/storage_manager.rel ^
+engine/tile_manager.rel ^
+object/level_object.rel ^
+object/scroll_object.rel ^
+object/storage_object.rel ^
+object/tile_object.rel ^
+screen/func_screen.rel screen/none_screen.rel screen/splash_screen.rel screen/test_screen.rel ^
+screen/title_screen.rel
 
 :: Execute
-ihx2sms output.ihx output.sms
+"C:\Program Files\SDCC\bin\ihx2sms.exe" output.ihx output.sms
+
+:: Copy
+copy output.sms ..\TestBuild.sms
 
 :: Delete
+cd banks
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+cd ..
+::cd content
+::if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+::cd ..
+
 cd devkit
 if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
@@ -52,10 +116,21 @@ cd engine
 if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
-if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
-if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
-:: if exist "*.noi"  del "*.noi"  > nul
+cd object
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+cd ..
+
+cd screen
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+cd ..
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.ihx" del "*.ihx" > nul
+if exist "*.lk"  del "*.lk"  > nul
+if exist "*.lst" del "*.lst" > nul
+::if exist "*.noi" del "*.noi" > nul
+if exist "*.sym" del "*.sym" > nul
 
 :: Run
-::java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
-output.sms
+java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
+::C:/SEGA/Fusion/Fusion.exe output.sms
